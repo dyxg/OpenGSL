0ea851624ff4b4eacaf12478102a043c3ec25ca9
dyxg <zxy.hhhh.cn@gmail.com>
2023-08-13 04:21:55 +0800

    used DeepRobust

--- opengsl/data/preprocess/attack.py
+++ opengsl/data/preprocess/attack.py
@@ -1,66 +1,35 @@
-from opengsl.method.models.metattack import Metattack
-from opengsl.method.models.nettack import Nettack
-from opengsl.method.models.gcn import GCN
 import torch
 import torch.nn.functional as F
 from opengsl.utils.utils import accuracy
 from sklearn.metrics import roc_auc_score
 from copy import deepcopy
 from opengsl.data.preprocess.normalize import normalize
-from opengsl.utils.utils import scipy_sparse_to_sparse_tensor
+from opengsl.utils.utils import scipy_sparse_to_sparse_tensor, sparse_tensor_to_scipy_sparse
 import numpy as np
+from deeprobust.graph.defense import GCN
+from deeprobust.graph.global_attack import MetaApprox, Metattack, Random
+from deeprobust.graph.targeted_attack import Nettack
+from tqdm import tqdm
 
-def train_surrogate(nhid, nclass, nlayers, dropout, epochs, lr, weight_decay, feats, adj, labels, train_mask, val_mask,  device):
-    loss_fn = F.binary_cross_entropy_with_logits if nclass == 1 else F.cross_entropy
-    metric = roc_auc_score if nclass == 1 else accuracy
-    surrogate = GCN(feats.shape[1], nhid, nclass, nlayers, dropout).to(device)
-    surrogate_optim = torch.optim.Adam(surrogate.parameters(), lr=lr,weight_decay=weight_decay)
-    loss_fn = F.binary_cross_entropy_with_logits if nclass == 1 else F.cross_entropy
-    metric = roc_auc_score if nclass == 1 else accuracy
-    best_val = 0
-    for epoch in range(epochs):
-        surrogate.train()
-        surrogate_optim.zero_grad()
-
-        # forward and backward
-        output = surrogate([feats, adj, True])
-        loss_train = loss_fn(output[train_mask], labels[train_mask])
-        acc_train = metric(labels[train_mask].cpu().numpy(),
-                                output[train_mask].detach().cpu().numpy())
-        acc_val = metric(labels[val_mask].cpu().numpy(), output[val_mask].detach().cpu().numpy())
-        if acc_val > best_val:
-            best_val = acc_val
-            weights = deepcopy(surrogate.state_dict())
-        loss_train.backward()
-        surrogate_optim.step()
-    surrogate.load_state_dict(weights)
-    return surrogate
 
 
 def metattack(adj, feats, labels, nclass, train_mask, val_mask, test_mask, conf, device):
+    """
+    adj : dense tensor
+    
+    """
+    
     attack_structure = conf.attack['attack_structure']
     attack_features = conf.attack['attack_features']
-    undirected = conf.attack['undirected']
-    with_bias = conf.attack['with_bias']
     lambda_ = conf.attack['lambda_']
-    train_iters = conf.attack['train_iters']
-    lr = conf.attack['lr']
-    momentum = conf.attack['momentum']
-    if conf.dataset['normalize']:
-        gcn_adj = normalize(adj.to_sparse(), add_loop=conf.dataset['add_loop'], sparse=conf.dataset['sparse']).to_dense()
-    else:
-        gcn_adj = adj
-    surrogate = train_surrogate(conf.attack['gcn_nhid'], nclass, conf.attack['gcn_layers'], conf.attack['gcn_dropout'], conf.attack['gcn_epochs'], 
-                                conf.attack['gcn_lr'], conf.attack['gcn_weight_decay'], feats, gcn_adj, labels, train_mask,val_mask, device)
-
-    
-    model = Metattack(model=surrogate, with_relu = True, nnodes = adj.shape[0], feature_shape = feats.shape,  
-                      attack_structure = attack_structure, attack_features = attack_features, undirected = undirected, device = device, lambda_ = lambda_, train_iters = train_iters,
-                      with_bias = with_bias, lr = lr, momentum = momentum)
-    model.attack(feats, adj, labels, train_mask, np.union1d(val_mask,test_mask), conf.attack['n_perturbations'], conf.attack['ll_constraint'], conf.attack['ll_cutoff'])
-    
-    # new_adj = adj
-    # new_feats = feats
+    ptb_rate = conf.attack['ptb_rate']
+    idx_unlabeled = np.union1d(val_mask, test_mask)
+    perturbations = int(ptb_rate * (adj.sum()//2))
+    surrogate = GCN(nfeat=feats.shape[1], nclass=nclass, nhid=16,
+            dropout=0.5, with_relu=False, with_bias=True, weight_decay=5e-4, device=device).to(device)
+    surrogate.fit(feats, adj, labels, train_mask)
+    model = Metattack(model=surrogate, nnodes=adj.shape[0], feature_shape=feats.shape,  attack_structure=attack_structure, attack_features=attack_features, device=device, lambda_=lambda_).to(device)
+    model.attack(feats, adj, labels, train_mask, idx_unlabeled, perturbations, ll_constraint=False)
     if attack_structure == True :
         new_adj = model.modified_adj
     else :
@@ -69,29 +38,43 @@ def metattack(adj, feats, labels, nclass, train_mask, val_mask, test_mask, conf,
         new_feats = model.modified_features
     else :
         new_feats = feats
-    # print(new_feats)
-    # print(feats)
     return new_adj, new_feats
 
-def nettack(adj, feats, labels, nclass, target_node, train_mask, conf, device):
+def nettack(adj, feats, labels, nclass, target_node_list, train_mask, val_mask, test_mask, conf, device):
+    """
+    adj : dense tensor
+    
+    """
 
     attack_structure = conf.attack['attack_structure']
     attack_features = conf.attack['attack_features']
-    directed = conf.attack['directed']
-    surrogate = train_surrogate(conf.attack['gcn_nhid'], nclass, 2, conf.attack['gcn_dropout'], conf.attack['gcn_epochs'], 
-                                conf.attack['gcn_lr'], conf.attack['gcn_weight_decay'], feats, adj, labels, train_mask, device)
-
-    model = Nettack(model=surrogate, attack_structure = attack_structure, attack_features = attack_features, device = device)
-    model.attack(feats, adj, labels, target_node, conf.attack['n_perturbations'], directed, conf.attack['n_influencers'], conf.attack['ll_cutoff'], conf.attack['verbose'])
-
+    n_perturbations = conf.attack['n_perturbations']
+    surrogate = GCN(nfeat=feats.shape[1], nclass=labels.max().item()+1,
+                    nhid=16, dropout=0, with_relu=False, with_bias=False, device=device).to(device)
+    surrogate.fit(feats, adj, labels, train_mask, val_mask, patience=30)
+    degrees = adj.sum(0)
     new_adj = adj
     new_feats = feats
-    if attack_structure == True :
-        new_adj = scipy_sparse_to_sparse_tensor(model.modified_adj).to(device)
-    else :
-        new_adj = adj
-    if attack_features == True : 
-        new_feats = model.modified_features
-    else :
-        new_feats = feats
+    for target_node in tqdm(target_node_list):
+        model = Nettack(surrogate, nnodes=adj.shape[0], attack_structure=attack_structure, attack_features=attack_features, device=device).to(device)
+        model.attack(new_feats, new_adj, labels, target_node, n_perturbations, verbose=False)
+        if attack_structure == True :
+            new_adj = scipy_sparse_to_sparse_tensor(model.modified_adj).to_dense().to(device)
+        if attack_features == True : 
+            new_feats = scipy_sparse_to_sparse_tensor(model.modified_features).to_dense().to(device)
     return new_adj, new_feats
+
+def select_nodes(adj, test_mask, p):
+    degrees = adj.sum(0)
+    target_node_list = []
+    for node in test_mask:
+        if (degrees[node] >= 10):
+            target_node_list.append(node)
+    return target_node_list
+
+def random_attack(adj, conf, device):
+    model = Random()
+
+    n_perturbations = int(conf.attack['ptb_rate'] * (adj.sum()//2))
+    model.attack(sparse_tensor_to_scipy_sparse(adj), n_perturbations)
+    return scipy_sparse_to_sparse_tensor(model.modified_adj).to_dense().to(device)
